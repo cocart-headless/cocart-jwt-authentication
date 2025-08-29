27e5a1127d6840fa099887c9067fc603e196b9fd
Sebastien <sebastiendumont1986@gmail.com>
2025-08-30 01:27:51 +0200

    Bumped build

--- includes/class-cocart-jwt-authentication.php
+++ includes/class-cocart-jwt-authentication.php
@@ -29,7 +29,7 @@ final class Plugin {
 	 *
 	 * @var string
 	 */
-	public static $version = '3.0.0-beta.1';
+	public static $version = '3.0.0-beta.2';
 
 	/**
 	 * Single instance of the CoCart class
